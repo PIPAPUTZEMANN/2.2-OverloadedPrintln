b924c5a9c06d8fd3958696350012ca0351b366c0
PIPAPUTZEMANN <sebi-brenner@web.de>
2017-08-15 15:44:04 +0200

    fertig

---
+++ 2.3 Obama/src/Obama.java
@@ -0,0 +1,16 @@
+
+public class Obama {
+
+	public static void main(String[] args) {
+		
+		String name = "Obama";	//Name
+		int age = 48;		//Alter
+		double income = 4000000;	//Einkommen
+		char gender = 'm';	//Geschlecht
+		boolean isPresident = true;	//Ist Präsident?
+		boolean isVegetarian = false;	//Ist Vegetarier?
+
+
+	}
+
+}
