c01d2e8306cce4f167d9da3688be65099c7120ac
PIPAPUTZEMANN <sebi-brenner@web.de>
2017-08-15 15:38:01 +0200

    fertig

---
+++ 2.3 FirstVariable/src/FirstVariable.java
@@ -0,0 +1,14 @@
+
+public class FirstVariable {
+
+	public static void main(String[] args) {
+		String name;	//Name
+		int age;		//Alter
+		double income;	//Einkommen
+		char gender;	//Geschlecht
+		boolean isPresident;	//Ist Präsident?
+		boolean isVegetarian;	//Ist Vegetarier?
+
+	}
+
+}
