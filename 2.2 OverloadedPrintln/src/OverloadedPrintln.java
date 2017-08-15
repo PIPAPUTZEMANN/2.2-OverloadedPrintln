a537ad4e4b101d975be3b19dfbc8c23eff9204f8
PIPAPUTZEMANN <sebi-brenner@web.de>
2017-08-15 14:50:07 +0200

    2

---
+++ 2.2 OverloadedPrintln/src/OverloadedPrintln.java
@@ -0,0 +1,13 @@
+
+public class OverloadedPrintln {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Verhaftet die üblichen Verdächtigen!");
+		System.out.println(true);
+		System.out.println(-273);
+		System.out.println();
+		System.out.println(1.64576746456767456435);
+	}
+
+}
