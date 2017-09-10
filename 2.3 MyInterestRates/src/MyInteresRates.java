7e29817a14fa0a47823237f1df94a1ce8e0e35b0
PIPAPUTZEMANN <sebi-brenner@web.de>
2017-09-10 09:03:36 +0200

    fertig

--- 2.3 MyInterestRates/src/MyInteresRates.java
+++ 2.3 MyInterestRates/src/MyInteresRates.java
@@ -2,8 +2,18 @@
 public class MyInteresRates {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Kaptial?");
+		double capital = new java.util.Scanner(System.in).nextDouble();
 
+		
+		System.out.println("Zinssatz?");
+		double interestRate = new java.util.Scanner(System.in).nextDouble();
+	
+		System.out.println("Anlagedauer in Monaten?");
+		int month = new java.util.Scanner(System.in).nextInt();
+		
+		double totalInterestRate = capital * interestRate * month / (12 * 100);
+		System.out.printf("Zinsen: %g%n", totalInterestRate);
 	}
 
 }
